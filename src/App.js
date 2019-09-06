22ff64314a60892b0e558ef43a675bc4ec04017d
Tomi <tomi.holstila@gmail.com>
2019-09-06 22:40:31 +0200

    mapbox boilerplate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map';
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 function App() {
+  React.useEffect(() => {
+    console.log(process.env)
+  })
+  const opts = {
+      mapbox: mapboxgl,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+  }
+  
   return (
     <div className="App">
       <header className="App-header">        
@@ -10,6 +20,7 @@ function App() {
           Kosovo elections
         </p>        
           Here be map        
+          <Map opts={opts}/>
       </header>
     </div>
   );
---
+++ src/components/Map.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import ReactMapGL from 'react-map-gl';
+
+export default class Map extends Component {
+
+  state = {
+    viewport: {
+      width: 400,
+      height: 400,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    }
+  };
+
+  render() {
+    console.log(this.props);
+    
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+        mapboxApiAccessToken={this.props.opts.accessToken}
+      />
+    );
+  }
+}
